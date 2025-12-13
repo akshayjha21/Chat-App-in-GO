e38617eeeaa3b6fcc13f145c1f1c8f58a8c46a33
akshay <akshayjha2111@gmail.com>
2025-12-13 11:52:41 +0530

    updating the type

---
+++ Backend/internal/storage/storage.go
@@ -0,0 +1,8 @@
+package storage
+
+import "github.com/akshayjha21/Chat-App-in-GO/internal/types"
+
+// import "os/user"
+type Storage interface {
+	CreateConnection(user *types.User,room *types.Room) (*types.RoomMember,error)
+}
--- Backend/internal/types/types.go
+++ Backend/internal/types/types.go
@@ -1,19 +1,20 @@
 package types
 
-type User struct{
-	Id uint `json:"Id" gorm:"primaryKey"`
-	Name string `json:"Name" validate:"required"`
-	Email string `json:"Email"`
+import "time"
+
+type User struct {
+	Id        uint   `json:"Id" gorm:"primaryKey"`
+	Name      string `json:"Name" validate:"required"`
+	Email     string `json:"Email" gorm:"unique;not null"`
+	CreatedAt time.Time `json:"created_at"`
 }
-
-type Room struct{
-	Id uint `json:"Id"`
-	Name string `json:"Name"`
+type Room struct {
+	Id        uint   `json:"Id"`
+	Name      string `json:"Name"`
+	Isprivate bool   `json:"Is_private" gorm:"default:false"`
 }
-
-
-type RoomMember struct{
-	RoomId uint32 `json:"RoomID"`
-	UserId uint32 `json:"UserID"`
-	Role string
+type RoomMember struct {
+	RoomId uint32 `json:"RoomID" gorm:"primaryKey"`
+	UserId uint32 `json:"UserID" gorm:"primaryKey"`
+	Role   string `json:"Role" gorm:"default:'member'"`
 }
