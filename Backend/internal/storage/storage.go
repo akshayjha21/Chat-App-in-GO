46736a61e2ac913980be02278db9c34a1618ab0c
akshay <akshayjha2111@gmail.com>
2026-01-16 13:48:28 +0530

    Ok

--- Backend/internal/storage/storage.go
+++ Backend/internal/storage/storage.go
@@ -6,4 +6,10 @@ import "github.com/akshayjha21/Chat-App-in-GO/Backend/internal/types"
 type Storage interface {
 	RegisterUser(user *types.User)(*types.User,error)
 	CreateConnection(user *types.User,room *types.Room) (*types.RoomMember,error)
+	GetUser(user  *types.User)(*types.User,error)
+	RegisterRoom(chatRoom *types.Room)(*types.Room,error)
+	GetRoom(code string) (*types.Room, error)
+	GetUserRooms(userID uint) ([]types.Room, error) 
+	CheckExistingMembers(userid uint, roomid uint) (*types.RoomMember, error) 
+
 }
